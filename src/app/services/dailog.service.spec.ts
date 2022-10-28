45a91508cc8c5f300db89ea0a37e9c0e9964e85e
F025XJE <F025XJE@tntad.fedex.com>
2022-10-28 18:16:33 +0530

    initial commit

--- src/app/services/dailog.service.spec.ts
+++ src/app/services/dailog.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClient } from '@angular/common/http';
 import { DailogService } from './dailog.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 describe('DailogService', () => {
   let service: DailogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [HttpClient]
+    });
     service = TestBed.inject(DailogService);
   });
 
